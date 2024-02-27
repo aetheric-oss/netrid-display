23d4c42b64024a69636ede822dbe573e0e59ac58
Alex M. Smith <smitham42@gmail.com>
2024-02-27 23:36:34 +0100

    feat: add troisjs with box demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from 'troisjs'
+
+function mounted() {
+  const renderer = this.$refs.renderer
+  const box = this.$refs.box.mesh
+  renderer.onBeforeRender(() => {
+    box.rotation.x += 0.01
+  })
+}
 </script>
 
 <template>
@@ -15,6 +24,16 @@ import TheWelcome from './components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+
+  <Renderer ref="renderer">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
 </template>
 
 <style scoped>
